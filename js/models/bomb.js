b810fa2f6ad98da90a7ad1f4f26f6f47b01aaead
Nick Rivadeneira <nicholas.rivadeneira@gmail.com>
2013-09-20 18:03:36 -0400

    Preliminary Bomb validation.

--- js/models/bomb.js
+++ js/models/bomb.js
@@ -1,4 +1,23 @@
 Bomb = Backbone.Model.extend({
   idAttribute: '_id',
-  urlRoot: 'http://localhost:9292/bombs'
+  urlRoot: 'http://localhost:9292/bombs',
+  validate: function(attributes){
+    if(!attributes.timestamp){
+      return 'A timestamp is required.';
+    }
+
+    if(!attributes.url){
+      return 'A URL is required.';
+    }
+
+    if(!attributes.request_params){
+      return 'Request parameters are required.';
+    }
+
+    try{
+      JSON.parse(request_params);
+    }catch(err){
+      return 'JSON is invalid.';
+    }
+  }
 });
