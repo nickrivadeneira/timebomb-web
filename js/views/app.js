f7fd10f5d11c1867a23fdf85edf41c50ae71f9fc
Nick Rivadeneira <nicholas.rivadeneira@gmail.com>
2013-09-23 17:18:48 -0400

    Automatically clear existing alerts when creating new ones.

--- js/views/app.js
+++ js/views/app.js
@@ -2,6 +2,7 @@ AppView = Backbone.View.extend({
   el: '#timebombapp',
   renderAlert: function(message, alertType){
     alertType || (alertType = "info");
+    this.clearAlerts();
     $('<div class="alert alert-' + alertType + '">' + message + '</div>').hide().prependTo(this.el).fadeIn();
   },
   clearAlerts: function(){
